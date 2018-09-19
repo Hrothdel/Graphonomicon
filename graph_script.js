3aa1434d0833e7e40ff91f58460b1bb4e2a82ce9
Hrothdel <leo32pop@gmail.com>
2018-09-19 09:59:16 +0300

    Separate graph script

---
+++ graph_script.js
@@ -0,0 +1,49 @@
+const height = 700, width = 700;
+const middleX = height/2, middleY = width/2;
+
+const fontSize = 30;
+
+const canvas = document.getElementById("mainCanvas"),
+    ctx = canvas.getContext("2d");
+ctx.lineWidth = 5;
+ctx.font = fontSize + "px Arial";
+
+let node_nr = 3;
+let angle_increment;
+let distance = 200, radius = 40;
+let posX, posY, angle;
+
+function draw_graph(){
+  ctx.clearRect(0, 0, width, height);
+
+  angle_increment = Math.PI*2/node_nr;
+  angle = 0;
+
+  for(let i = 0; i < node_nr; i++){
+      posX = middleX + Math.cos(angle)*distance;
+      posY = middleY + Math.sin(angle)*distance;
+
+      ctx.beginPath();
+      ctx.arc(posX, posY, radius, 0, Math.PI*2);
+      ctx.fillText(i+1, posX-fontSize/3, posY+fontSize/3);
+      ctx.stroke();
+
+      angle += angle_increment;
+  }
+}
+
+function add_node(){
+  node_nr++;
+
+  draw_graph();
+}
+
+function remove_node(){
+  if(node_nr){
+    node_nr--;
+
+    draw_graph();
+  }
+}
+
+draw_graph();
