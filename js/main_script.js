ea4cc53fdb2773789aac5758ce34b41c380aa26a
Hrothdel <leo32pop@gmail.com>
2019-03-14 13:22:50 +0200

    Clean up global variables

--- js/main_script.js
+++ js/main_script.js
@@ -1,46 +1,20 @@
 const height = 700, width = 700;
-const middleX = height/2, middleY = width/2;
-
-const fontSize = 30;
+const middleX = height/2,
+      middleY = width/2;
 
 const canvas = document.getElementById("mainCanvas"),
       ctx = canvas.getContext("2d");
 
-let node_nr = 6;
-let angle_increment;
-let distance = 200, radius = 40;
+let node_nr = 6, distance = 200,
+    radius = 40, fontSize = 30;
 
-let neighbour_matrix = [];
+let angle_increment,
+    neighbour_matrix = [];
 
 let tree = 0, directed = 1;
 
-let root = 0;
-
-let tree_rows = [
-  [0],
-  [1, 2, 4],
-  [3, 5]
-];
-
-let parents;
-
-let children = [
-  [1, 2, 4],
-  [],
-  [],
-  [],
-  [3, 5],
-  []
-];
-
-let tree_position = [
-  {"x": 0, "y": 0},
-  {"x": 0, "y": 1},
-  {"x": 1, "y": 1},
-  {"x": 0, "y": 2},
-  {"x": 2, "y": 1},
-  {"x": 1, "y": 2}
-];
+let root = 0, tree_rows, parents,
+    children, tree_position;
 
 let arrow_color = "#f77",
     line_color = "#000",
