773538dcc6318f3551ed51b3432da0b665b4c93f
Hrothdel <leo32pop@gmail.com>
2019-03-26 10:02:58 +0200

    Add create_initialized_matrix function

--- js/utility_functions.js
+++ js/utility_functions.js
@@ -2,3 +2,17 @@ function move_at_angle(posX, posY, angle, move_distance){
   return [posX + Math.cos(angle) * move_distance,
           posY + Math.sin(angle) * move_distance];
 }
+
+function create_initialized_matrix(rows, columns, initial_value){
+  let matrix = [];
+
+  for(let i = 0; i < rows; i++){
+    matrix[i] = [];
+
+    for(let j = 0; j < columns; j++){
+      matrix[i][j] = initial_value;
+    }
+  }
+
+  return matrix;
+}
